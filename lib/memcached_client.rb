c2ddf37449c4f96b3f13e3cf2180405730e65606
Hernan Yanez <heryanez212@gmail.com>
2021-03-12 17:46:14 -0300

    fixed client for testing

--- lib/memcached_client.rb
+++ lib/memcached_client.rb
@@ -5,23 +5,50 @@ require 'socket'
 # Memcache client implementation
 class MemcachedClient
   def initialize
-    @hostname = 'localhost' # Default for memcache
+    @hostname = '127.0.0.1' # Default for memcache
     @port = 11_211
-    @socket = TCPSocket.open(@hostname, @port)
   end
 
   def next_line_readable?(_socket)
     select([@socket], nil, nil, 0.1)
   end
 
-  def start
-    @socket.puts('set tutorialspoint 0 900 9')
-    @socket.puts('memcached')
-    puts(@socket.gets.chop)
-    @socket.puts('get sss tutorialspoint')
+  def read
     puts(@socket.gets.chop) while next_line_readable?(@socket)
   end
-end
 
-client = MemcachedClient.new
-client.start
+  def write(line)
+    @socket.puts(line) unless close?(line)
+  end
+
+  def connect
+    @socket = TCPSocket.open(@hostname, @port)
+    puts('Connected to localhost:11211')
+    true
+  rescue Errno::ECONNREFUSED || Errno::ETIMEDOUT
+    puts('Error: Connection timeout or refused')
+    false
+  end
+
+  def close?(line)
+    if %w[close quit].include?(line)
+      disconnect
+      true
+    else
+      false
+    end
+  end
+
+  def disconnect
+    @socket.close
+  end
+
+  def start
+    return unless connect
+
+    loop do
+      write(gets)
+      read
+    end
+  end
+end
